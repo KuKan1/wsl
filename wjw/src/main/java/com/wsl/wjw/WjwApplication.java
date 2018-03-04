fa0e5403adeb9d8c4195bb9f85d45cb5f79db2b0
Liuzy <liuziyingbeidou@163.com>
2018-03-04 20:45:30 +0800

    网劵网-初始化工程

---
+++ wjw/src/main/java/com/wsl/wjw/WjwApplication.java
@@ -0,0 +1,12 @@
+package com.wsl.wjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjwApplication.class, args);
+	}
+}
