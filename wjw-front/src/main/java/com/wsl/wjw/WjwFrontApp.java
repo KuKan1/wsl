e132fd046f14df55fffe5ef965e99110fd98508b
Liuzy <liuziyingbeidou@163.com>
2018-03-05 21:34:26 +0800

    wjw-front 工程初始化

---
+++ wjw-front/src/main/java/com/wsl/wjw/WjwFrontApp.java
@@ -0,0 +1,12 @@
+package com.wsl.wjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjwFrontApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjwFrontApp.class, args);
+	}
+}
---
+++ wjw-front/src/test/java/com/wsl/wjw/WjwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wsl.wjw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WjwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
