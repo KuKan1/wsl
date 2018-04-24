fad18c7537be5e05f91a5f393946afafa1d370b0
Liuzy <liuziyingbeidou@163.com>
2018-04-24 22:01:34 +0800

    前端完成大概

--- wjw-front/src/main/resources/static/pageJs/show/inner_page.js
+++ wjw-front/src/main/resources/static/pageJs/show/inner_page.js
@@ -3,20 +3,62 @@
  */
 
 $(function(){
-	$(".category_id").on("click",loadData);
+	$(".category_id").bind("click",loadDataByNoParam);
+	var ct_id = $(".inner_nav").find('li').each(function(){
+		if($(this).hasClass("inner_con_style")){
+			var type_id = $(this).attr("ty-id");
+			var category_id = $(this).attr("ct-id");
+			loadData(type_id,category_id);
+		}
+	});
+	
 });
 
-function loadData(){
-	var category_id = $(this).attr("ct-id");debugger
-	var type_url = CommonVar.app_root+"/shop/all";
+function loadDataByNoParam(){
+	var type_id = $(this).attr("ty-id");
+	var category_id = $(this).attr("ct-id");
+	doLoadData(type_id,category_id)
+}
+
+function loadData(type_id,category_id){
+	if(type_id == undefined){
+		type_id = $(this).attr("ty-id");
+	}
+	if(category_id == undefined){
+		category_id = $(this).attr("ct-id");
+	}
+	doLoadData(type_id,category_id)
+}
+
+function doLoadData(type_id,category_id){
+	var type_url = CommonVar.app_root+"/shop/"+type_id+"/"+category_id;
+	$("#sw-shop-list").empty();
 	$.get(type_url, function(data){
-		for (var i = 0; i < data.length; i++) {
-			var type = data[i];
-			var em_a = '<a href="mesh/Inner_page/'+type.id+'">';
-			var em_img = em_a+'<img src="images/'+type.vshort+'.png" />';
-			var em_span = em_img+'<span>'+type.vname+'</span>';
-			var em =em_span+ '</a>';
-			$("#index_lan").append(em);
+		var step = 2;
+		var len = data.length;
+		for (var i = 0; i < len; i+=step) {
+			var left_data = data[i];
+			var em_left = '<a href="'+left_data.vurl+'">'+
+				'<li class="border_right fl">'+
+				'<img src="'+left_data.vcover+'" />'+
+				'<span>'+left_data.vname+'</span>'+
+				'<span class="int_color">京东价: ￥'+left_data.originalPrice+'</span>'+
+				'<span class="int_color">券后价: ￥'+left_data.couponPrice+'</span>'+
+				'</li>'+
+				'</a>';
+			$("#sw-shop-list").append(em_left);
+			if((i+1) < len){
+				var right_data = data[i+1];
+				var em_right = '<a href="'+right_data.vurl+'">'+
+					'<li class="fl">'+
+					'<img src="'+right_data.vcover+'" />'+
+					'<span>'+right_data.vname+'</span>'+
+					'<span class="int_color">京东价: ￥'+right_data.originalPrice+'</span>'+
+					'<span class="int_color">券后价: ￥'+right_data.couponPrice+'</span>'+
+					'</li>'+
+					'</a>';
+				$("#sw-shop-list").append(em_right);
+			}
 		}
 	});
 }
