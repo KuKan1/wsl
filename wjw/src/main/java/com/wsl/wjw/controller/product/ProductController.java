101d141c4ce022dbee4ef975593503deabfb27e3
Liuzy <liuziyingbeidou@163.com>
2018-05-08 22:39:43 +0800

    产品添加页

--- wjw/src/main/java/com/wsl/wjw/controller/product/ProductController.java
+++ wjw/src/main/java/com/wsl/wjw/controller/product/ProductController.java
@@ -1,12 +1,23 @@
 package com.wsl.wjw.controller.product;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wsl.wjw.dto.CategoryDto;
+import com.wsl.wjw.service.CategoryService;
+
 @Controller
 @RequestMapping(value = {"/product"})
 public class ProductController {
+	
+	@Autowired
+	private CategoryService categoryService;
 	
 	@RequestMapping(value = "/index")
     public String index(ModelAndView modelAndView) {
@@ -14,5 +25,16 @@ public class ProductController {
 		return "/product/product-list";  
     }
 
+	@RequestMapping(value = "/add")
+    public String add(HttpServletRequest request,Map<String,Object> map) throws Exception {
+		String id_param = request.getParameter("id");
+		if(id_param != null){
+			String id = id_param.split("[.]")[0];
+			CategoryDto categoryDto = categoryService.queryCategoryListById(Long.valueOf(id));
+			map.put("categoryDto", categoryDto);
+		}
+		return "/product/product-add";  
+    }
+	
     
 }
---
+++ wjw/src/main/resources/static/pageJs/product.category.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+
+$(function(){
+	
+	queryCategory();
+});
+
+function queryCategory(){
+	var category_url = CommonVar.app_root+"/category/list";
+	$.get(category_url, function(data){
+		createCategoryTree(data);
+	});
+}
+
+function createCategoryTree(zNodes){
+	var t = $("#treeCatetory");
+	t = $.fn.zTree.init(t, setting, zNodes);
+	demoIframe = $("#catetoryIframe");
+	//demoIframe.on("load", loadReady);
+	var zTree = $.fn.zTree.getZTreeObj("tree");
+	//zTree.selectNode(zTree.getNodeByParam("id",'11'));
+}
+
+var setting = {
+	view: {
+		dblClickExpand: false,
+		showLine: false,
+		selectedMulti: false
+	},
+	data: {
+		simpleData: {
+			enable:true,
+			idKey: "id",
+			pIdKey: "pId",
+			rootPId: ""
+		}
+	},
+	callback: {
+		beforeClick: function(treeId, treeNode) {
+			//var zTree = $.fn.zTree.getZTreeObj("tree");
+			if (treeNode.isParent) {
+				//zTree.expandNode(treeNode);
+				return false;
+			} else {
+				$("#catetoryIframe").attr("src",treeNode.file + ".html");
+				return true;
+			}
+		}
+	}
+};
+		
+var code;
+		
+function showCode(str) {
+	if (!code) code = $("#code");
+	code.empty();
+	code.append("<li>"+str+"</li>");
+}
