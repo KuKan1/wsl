4008842e40b16a7b79f34c7d6c6a7b227cf9aca8
Liuzy <liuziyingbeidou@163.com>
2018-05-12 10:30:36 +0800

    点击菜单类加载

--- wjw/src/main/java/com/wsl/wjw/controller/product/ProductController.java
+++ wjw/src/main/java/com/wsl/wjw/controller/product/ProductController.java
@@ -1,16 +1,21 @@
 package com.wsl.wjw.controller.product;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wsl.wjw.dto.CategoryDto;
+import com.wsl.wjw.dto.ShopDto;
 import com.wsl.wjw.service.CategoryService;
+import com.wsl.wjw.service.ShopService;
 
 @Controller
 @RequestMapping(value = {"/product"})
@@ -18,6 +23,8 @@ public class ProductController {
 	
 	@Autowired
 	private CategoryService categoryService;
+	@Autowired
+	private ShopService shopService;
 	
 	@RequestMapping(value = "/index")
     public String index(ModelAndView modelAndView) {
@@ -35,6 +42,12 @@ public class ProductController {
 		}
 		return "/product/product-add";  
     }
+	
+	@ResponseBody
+	@RequestMapping(value="/{typeId}/{categoryId}")
+	public List<ShopDto> queryShopByCategoryId(@PathVariable Long typeId,@PathVariable Long categoryId) throws Exception{
+		return shopService.queryShopByCategoryId(typeId, categoryId);
+	}
 	
     
 }
--- wjw/src/main/resources/static/pageJs/product.js
+++ wjw/src/main/resources/static/pageJs/product.js
@@ -3,13 +3,66 @@
  */
 
 $(function(){
-	
+	var type_id = 0;
+	var category_id = 0;
+	//查询产品
+	queryProduct(type_id,category_id);
+	//查询分类
 	queryCategory();
 });
 
+/**
+ * 查询产品
+ * @param type_id
+ * @param category_id
+ */
+function queryProduct(type_id,category_id){
+	var product_url = CommonVar.app_root+"/"+type_id+"/"+category_id;
+	$(".table > tbody").empty();
+	$.get(product_url, function(data){
+		debugger
+		var len = data.length;
+		for (var i = 0; i < len; i+=1) {
+			var pd = data[i];
+			var pd_em = '<tr class="text-c va-m">'+
+				'<td><input name="" type="checkbox" value=""></td>'+
+				'<td>001</td>'+
+				'<td>'+
+				'<a onClick="product_show("哥本哈根橡木地板","product-show.html","10001")" href="javascript:;">'+
+				'<img width="60" class="product-thumb" src="temp/product/Thumb/6204.jpg">'+
+				'</a>'+
+				'</td>'+
+				'<td class="text-l">'+
+				'<a style="text-decoration:none" onClick="product_show("哥本哈根橡木地板","product-show.html","10001")" href="javascript:;">'+
+				'<img title="国内品牌" src="static/h-ui.admin/images/cn.gif"> '+
+				'<b class="text-success">圣象</b> 哥本哈根橡木地板KS8373'+
+				'</a>'+
+				'</td>'+
+				'<td class="text-l">原木的外在,实木条形结构,色泽花纹自然,写意;款式设计吸取实木地板的天然去雕饰之美,在视觉上给人带来深邃联想.多款产品适合搭配不同的风格的室内装饰;功能流露出尊贵典雅的大气韵味。</td>'+
+				'<td><span class="price">356.0</span> 元/平米</td>'+
+				'<td class="td-status"><span class="label label-success radius">已发布</span></td>'+
+				'<td class="td-manage">'+
+				'<a style="text-decoration:none" onClick="product_stop(this,"10001")" href="javascript:;" title="下架">'+
+				'<i class="Hui-iconfont">&#xe6de;</i>'+
+				'</a> '+
+				'<a style="text-decoration:none" class="ml-5" onClick="product_edit("产品编辑","product-add.html","10001")" href="javascript:;" title="编辑">'+
+				'<i class="Hui-iconfont">&#xe6df;</i>'+
+				'</a> '+
+				'<a style="text-decoration:none" class="ml-5" onClick="product_del(this,"10001")" href="javascript:;" title="删除">'+
+				'<i class="Hui-iconfont">&#xe6e2;</i>'+
+				'</a>'+
+				'</td></tr>';
+			$(".table > tbody").append(pd_em);
+		}
+	});
+}
+
+/**
+ * 查询分类 
+ **/
 function queryCategory(){
 	var category_url = CommonVar.app_root+"/category/list";
-	$.get(category_url, function(data){debugger
+	$.get(category_url, function(data){
 		createCategoryTree(data);
 	});
 }
@@ -50,11 +103,4 @@ var setting = {
 		}
 	}
 };
-		
-var code;
-		
-function showCode(str) {
-	if (!code) code = $("#code");
-	code.empty();
-	code.append("<li>"+str+"</li>");
-}
+
